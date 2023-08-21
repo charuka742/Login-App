9b163e9b1e9f4acbae3315f5e3b12a41621c8584
Disanayaka AAC <charair742@gmail.com>
2023-08-21 23:03:13 +0530

    UI done

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl font-bold text-gray-800"> PAGE NOT FOUND</h1>
+    </div>
   )
 }
